c5683245e6a18c9de0f19613bbd3422f3f618724
liquid-O2 <this.is.arunava.b@gmail.com>
2021-04-28 18:41:36 +0530

    used json server

--- src/views/job/Details.vue
+++ src/views/job/Details.vue
@@ -1,20 +1,35 @@
 <template>
-  <h1>Job Details</h1>
-  <p>Job Id is {{id}}</p>
+  <div v-if="job">
+    <h1>Job Details</h1>
+    <p>Job Id is {{ id }}</p>
+  </div>
+  <div v-else>
+    <p>Loading Details</p>
+  </div>
 </template>
 
 <script>
 export default {
-    props:['id'],
-//   data(){
-      
-//       return{
-//           id:this.$route.params.id
-//       }
-//   }
-}
+  props: ["id"],
+  data() {
+    return {
+      job: null,
+    };
+  },
+  mounted() {
+    fetch("http://localhost:3000/jobs" + this.id)
+      .then((res) => res.json())
+      .then((data) => (this.job = data))
+      .catch((err) => console.log(err.message));
+  },
+  //   data(){
+
+  //       return{
+  //           id:this.$route.params.id
+  //       }
+  //   }
+};
 </script>
 
 <style>
-
 </style>
--- src/views/job/Jobs.vue
+++ src/views/job/Jobs.vue
@@ -1,41 +1,47 @@
 <template>
-  <h1>Jobs</h1>
-  <div v-for="job in jobs" :key="job.id">
-    <router-link :to="{name: 'Details',  params:{id:job.id}}">
-    <h3>{{job.title}}</h3>
-    </router-link>
+  <div v-if="jobs.length">
+    <h1>Jobs</h1>
+    <div v-for="job in jobs" :key="job.id">
+      <router-link :to="{ name: 'Details', params: { id: job.id } }">
+        <h3>{{ job.title }}</h3>
+      </router-link>
+    </div>
+  </div>
+  <div v-else>
+    <p>Jobs Loading ....</p>
   </div>
 </template>
 
 <script>
 export default {
-   data(){
-       return{
-           jobs:[
-               {title:'Web Developer', id:1, desc:'lorem'},
-               {title:'Web Designer', id:2, desc:'lorem'},
-               {title:'Manager', id:3, desc:'lorem'}
-           ]
-       }
-   
-}
-}
+  data() {
+    return {
+      jobs: [],
+    };
+  },
+  mounted() {
+    fetch("http://localhost:3000/jobs")
+      .then((res) => res.json())
+      .then((data) => (this.jobs = data))
+      .catch((err) => console.log(err.message));
+  },
+};
 </script>
 
 <style scoped>
- h3{
-    margin: auto;
-    margin:1rem auto;
-    padding: 1rem;
-    border-radius: 1rem;
-    background: #dfdfdf;
-    width: 20%;
-    color: #2c3e50;
-    font-weight: bold;
-    font-size: 1.4rem;
+h3 {
+  margin: auto;
+  margin: 1rem auto;
+  padding: 1rem;
+  border-radius: 1rem;
+  background: #dfdfdf;
+  width: 20%;
+  color: #2c3e50;
+  font-weight: bold;
+  font-size: 1.4rem;
 }
-a{
-    color: #2c3e50;
-    text-decoration:none;
+a {
+  color: #2c3e50;
+  text-decoration: none;
 }
 </style>
